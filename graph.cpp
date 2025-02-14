e10f07570f24847fd1b47166024c7a2c48f3ae03
DillFennel <julea.basik@gmail.com>
2025-02-14 18:41:56 +0300

    Доработана структура графов

--- graph.cpp
+++ graph.cpp
@@ -7,16 +7,40 @@ class Graph{
         int value;
         std::list <Graph*> parents;
     public:
-        add_parents(Graph* parent, ...){
-            va_list val;
-            va_start(val, parents);
-            for(auto i : parents){
-                parents.push_back(va_arg(val, Graph*));
-            }
-            va_end(val);
+        void add_parent(Graph* inp_parent){
+            parents.push_back(inp_parent);
         }
+        Graph* get_parent(int n){
+            std::list<Graph*>::iterator ret = parents.begin();
+            for(int i=0; i<n; i++){
+                ret++;
+            }
+            return *ret;
+        }
+        Graph(int inp_value){
+            value = inp_value;
+        }
+        Graph(int inp_value, Graph* inp_parent){
+            value = inp_value;
+            add_parent(inp_parent);
+        }
+        int get_value(){
+            return value;
+        }
+        std::pair<int, int> get_longest_path(){
+            int ret = 0;
+            int longest_parent = 0;
+            for(auto i: parents){
+                std::pair<int, int> parent_data = get_longest_path()
+                if()
+            }
+        }
+
 };
 int main(){
-    std::cout<<1<<std::endl;
+    Graph a (1);
+    Graph b (2, &a);
+
+
     return 1;
 }
